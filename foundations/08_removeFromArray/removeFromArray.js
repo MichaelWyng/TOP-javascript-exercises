b50c39d9febf17c94f0cb58caf08a8bfde411afb
Michael <244812869+MichaelWyng@users.noreply.github.com>
2026-01-30 20:22:09 -0500

    Complete exercise 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+  let result = [];
+
+  for (let value of arr) {
+    if (!toRemove.includes(value)) {
+      result.push(value);
+    }
+  }
+
+  return result;
 };
 
 // Do not edit below this line
